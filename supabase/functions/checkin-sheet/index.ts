c1c8fc0a1bb2fbc19ba88247463209155bfff0bb
DRACula <c.daedalus@wustl.edu>
2024-10-12 01:56:47 -0500

    printing outside async

--- supabase/functions/checkin-sheet/index.ts
+++ supabase/functions/checkin-sheet/index.ts
@@ -7,6 +7,9 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 import { serve } from 'https://deno.land/std@0.177.0/http/server.ts'
 import { corsHeaders } from '../_shared/cors.ts'
 
+console.log(Deno.env.get('SUPABASE_URL'))
+console.log(Deno.env.get('SUPABASE_KEY'))
+
 serve(async (req: Request) => {
   console.log(process.env.SUPABASE_KEY);
   console.log(process.env.SUPABASE_URL);
