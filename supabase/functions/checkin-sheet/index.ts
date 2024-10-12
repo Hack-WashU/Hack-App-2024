1e89642f9eb43d8f54f3be9a8f1c5cacd08ce26c
DRACula <c.daedalus@wustl.edu>
2024-10-12 01:29:55 -0500

    trying supabase_key

--- supabase/functions/checkin-sheet/index.ts
+++ supabase/functions/checkin-sheet/index.ts
@@ -7,49 +7,49 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 import { serve } from 'https://deno.land/std@0.177.0/http/server.ts'
 import { corsHeaders } from '../_shared/cors.ts'
 
-// serve(async (req: Request) => {
-//   console.log(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-//   console.log(process.env.NEXT_PUBLIC_SUPABASE_URL);
-//   try {
+serve(async (req: Request) => {
+  console.log(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+  console.log(process.env.NEXT_PUBLIC_SUPABASE_URL);
+  try {
     
-//     const supabaseClient = createClient(
-//       // Supabase API URL - env var exported by default.
-//       //Deno.env.get('SUPABASE_URL') ?? '',
-//       process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
-//       // Supabase API ANON KEY - env var exported by default.
-//       //Deno.env.get('SUPABASE_KEY') ?? '',
-//       process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
-//       // Create client with Auth context of the user that called the function.
-//       // This way your row-level-security (RLS) policies are applied.
-//       { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
-//     )
-//     // Now we can get the session or user object
-//     const {
-//       data: { user },
-//     } = await supabaseClient.auth.getUser()
-//     console.log(user)
+    const supabaseClient = createClient(
+      // Supabase API URL - env var exported by default.
+      //Deno.env.get('SUPABASE_URL') ?? '',
+      process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+      // Supabase API ANON KEY - env var exported by default.
+      //Deno.env.get('SUPABASE_KEY') ?? '',
+      process.env.NEXT_PUBLIC_SUPABASE_KEY ?? '',
+      // Create client with Auth context of the user that called the function.
+      // This way your row-level-security (RLS) policies are applied.
+      { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
+    )
+    // Now we can get the session or user object
+    const {
+      data: { user },
+    } = await supabaseClient.auth.getUser()
+    console.log(user)
 
 
-//     const raw_auth = Deno.env.get("GOOGLE_OAUTH");
-//     const authProvider = new GoogleAuth()
+    const raw_auth = Deno.env.get("GOOGLE_OAUTH");
+    const authProvider = new GoogleAuth()
 
-//     const sheets = new Sheets(authProvider.fromJSON(JSON.parse(raw_auth!)));
+    const sheets = new Sheets(authProvider.fromJSON(JSON.parse(raw_auth!)));
 
-//     await sheets.spreadsheetsValuesAppend(
-//       "Checkin",
-//       Deno.env.get("SPREADSHEET"),
-//       { majorDimension: "ROWS", values: [[user.email]] },
-//       { includeValuesInResponse: true, valueInputOption: "USER_ENTERED" }
-//     )
-//   } catch (error) {
-//     return new Response(
-//       JSON.stringify(error),
-//       { headers: { ...corsHeaders, "Content-Type": "application/json" } },
-//     )
-//   }
+    await sheets.spreadsheetsValuesAppend(
+      "Checkin",
+      Deno.env.get("SPREADSHEET"),
+      { majorDimension: "ROWS", values: [[user.email]] },
+      { includeValuesInResponse: true, valueInputOption: "USER_ENTERED" }
+    )
+  } catch (error) {
+    return new Response(
+      JSON.stringify(error),
+      { headers: { ...corsHeaders, "Content-Type": "application/json" } },
+    )
+  }
 
-//   return new Response(
-//     JSON.stringify("Success!"),
-//     { headers: { ...corsHeaders, "Content-Type": "application/json" } },
-//   )
-// })
+  return new Response(
+    JSON.stringify("Success!"),
+    { headers: { ...corsHeaders, "Content-Type": "application/json" } },
+  )
+})
