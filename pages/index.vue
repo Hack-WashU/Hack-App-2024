e7a86f3306c8d52a994e9887b913e6ce0fe9b241
DRACula <c.daedalus@wustl.edu>
2024-10-12 02:08:42 -0500

    try printing in index

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <script>
-console.log(Deno.env.get('SUPABASE_URL'))
-console.log(Deno.env.get('SUPABASE_KEY'))
+import { serve } from 'https://deno.land/std@0.177.0/http/server.ts';
+serve(async () => {
+  console.log(Deno.env.get('SUPABASE_URL'));
+  console.log(Deno.env.get('SUPABASE_KEY'));
+})
 export default {
   mounted() {
     this.$router.push('/schedule');
