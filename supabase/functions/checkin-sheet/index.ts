cc5a1e9132b803d09687f7bf1c24206ce3bccc3d
DRACula <c.daedalus@wustl.edu>
2024-10-12 00:59:41 -0500

    trying anon_key

--- supabase/functions/checkin-sheet/index.ts
+++ supabase/functions/checkin-sheet/index.ts
@@ -15,7 +15,7 @@ serve(async (req: Request) => {
       process.env.SUPABASE_URL,
       // Supabase API ANON KEY - env var exported by default.
       //Deno.env.get('SUPABASE_KEY') ?? '',
-      process.env.SUPABASE_KEY,
+      process.env.SUPABASE_ANON_KEY,
       // Create client with Auth context of the user that called the function.
       // This way your row-level-security (RLS) policies are applied.
       { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
